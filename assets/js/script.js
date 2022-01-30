e6b31164a7b4ab3ce357cf4e9acf517c531cb61c
codenamehemingway <jared.hemingway.jh@gmail.com>
2022-01-30 12:00:24 -0700

    changed html text area id to numbers so i can reference them in the loop. Added for loop to change PPF colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,22 @@
 // global variables
 var currentTime = moment();
+var entryCount = 1;
 // display time on top of page
-$("#currentDay").text(
-  currentTime.format("dddd") + ", " + currentTime.format("MMM Do YYYY")
-);
+$("#currentDay").text(currentTime.format("HH:mm MMMM Do, YYYY"));
 
 // get local storage
 
-// color coding past/present/future
-
+// color coding past/present/future in mil time to avoid numerical ascension issues
+for (i = 8; i < 18; i++) {
+  var hour = currentTime.hour();
+  var tBlock = "#textblock" + entryCount;
+  if (i < hour) {
+    $(tBlock).addClass("past");
+  } else if (i > hour) {
+    $(tBlock).addClass("future");
+  } else {
+    $(tBlock).addClass("present");
+  }
+  entryCount++;
+}
 // save to storage function
